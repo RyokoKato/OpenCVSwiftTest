e413b4f854f5c0af2bb0eb9e35daa2d9570766b2
RyokoKato <amedama.1017@gmail.com>
2018-07-24 01:30:54 +0900

    add: setButtonStyle function

--- opencvTest/UIButtonExtension.swift
+++ opencvTest/UIButtonExtension.swift
@@ -14,13 +14,26 @@ class CustomButton: UIButton {
         self.setData()
     }
     
-    override required init?(coder aDecoder: NSCoder) {
+    required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         self.setData()
     }
     
     func setData() {
-        self.backgroundColor = UIColor.lightGray
+        self.backgroundColor = UIColor.gray02
         self.layer.cornerRadius = self.frame.size.height / 4.0
+    }
+}
+
+extension CustomButton {
+    func setButtonStyle(buttonStyle: ButtonStyles){
+        switch buttonStyle {
+        case .resetButton:
+            self.backgroundColor = UIColor.red02
+            self.setTitleColor(UIColor.white, for: .normal)
+        case .processButton:
+            self.backgroundColor = UIColor.gray01
+            self.setTitleColor(UIColor.gray03, for: .normal)
+        }
     }
 }
