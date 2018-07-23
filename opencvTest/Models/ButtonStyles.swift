0773f7944b90e30d547bfd6a80ad96934247e7fd
RyokoKato <amedama.1017@gmail.com>
2018-07-24 01:30:13 +0900

    add: add ButtonStyles enum

---
+++ opencvTest/Models/ButtonStyles.swift
@@ -0,0 +1,14 @@
+//
+//  ButtonStyle.swift
+//  opencvTest
+//
+//  Created by RyokoKato on 2018/07/23.
+//  Copyright © 2018年 加藤涼子. All rights reserved.
+//
+
+import Foundation
+
+enum ButtonStyles {
+    case resetButton    // 0
+    case processButton  // 1
+}
